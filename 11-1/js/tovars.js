df39bf8afb7678e1ad0ba2d4a5c7ede5181cafb9
Ekaterina <perovakatharina@gmail.com>
2023-12-01 20:28:14 +0300

    Задача №1 сделана

---
+++ 11-1/js/tovars.js
@@ -0,0 +1,40 @@
+'use strict'
+export let tovarsObj=[{
+    nameTovar:"Фитнес-браслет DEXP SB60",
+    imgSrc:"dexp_sb60.jpg",
+    corpus:"черный",
+    remen:"черный",
+    displaySize:"0.96\"",
+    displayType:"OLED",
+    blutoth:"true",
+    OS:{
+        os1:"Android 4.2",
+        os2:"iOS8",
+    }
+},
+{
+    nameTovar:"Смарт-часы RunGo W2",
+    imgSrc:"rungo_w2.jpg",
+    corpus:"зеленый",
+    remen:"зеленый",
+    displaySize:".3\"",
+    displayType:"TFT(TN)",
+    blutoth:"true",
+    OS:{
+        os1:"Android",
+        os2:"iOS",
+    }
+},
+{
+    nameTovar:"Смарт-часы Welbe Prime",
+    imgSrc:"welbe_prime.jpg",
+    corpus:"черный",
+    remen:"розовый",
+    displaySize:"1.69\"",
+    displayType:"IPS",
+    blutoth:"true",
+    OS:{
+        os1:"Android 5.0",
+        os2:"iOS 9",
+    }
+}]
